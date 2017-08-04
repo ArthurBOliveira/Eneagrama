7cc9a3dfdc5b719e28aafac3878251e5468ba199
Arthur B. de Oliveira <arthurboliveira93@gmail.com>
2017-08-04 15:18:43 -0300

    - First!

---
+++ Roleta/Assets/Scripts/Wheel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+    public float torque;
+
+    private Rigidbody2D rg2d;
+
+    private void Start()
+    {
+        rg2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void Spin()
+    {
+        float rnd = Random.Range(1, 10);
+
+        rg2d.AddTorque(torque * rnd, ForceMode2D.Impulse);
+    }
+
+    public void Stop()
+    {
+        rg2d.angularVelocity = 0;
+    }
+}
